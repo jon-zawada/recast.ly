92c0c364000058aeb71b436de8fc69857b72a6fb
jon-zawada <jonathan_zawada@yahoo.com>
2020-02-18 20:03:11 -0800

    Adds incomplete implementation for videolist

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,5 +1,12 @@
-var VideoList = () => (
+import exampleVideoData from '/src/data/exampleVideoData.js';
+import VideoListEntry from './components/VideoListEntry.js';
+
+
+var VideoList = (props) => (
+
+
   <div className="video-list">
+
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
@@ -13,6 +20,7 @@ var VideoList = () => (
 VideoList.propTypes = {
   videos: PropTypes.array.isRequired
 };
+ReactDOM.render(<VideoList/>, getElementsByClassName('col-md-5'));
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
